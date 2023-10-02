20a45b403394b47b1ecc5379caa9709e63272e71
memerax <josephwhite@gmx.com>
2023-10-02 19:13:40 -0230

    Server changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,28 @@
-import express from 'express'
+import express, {response} from 'express'
 
 const app = express();
+import {MongoClient} from "mongodb";
+
 app.use(express.json());
-app.post('/hello', (req,res) => {
-    console.log(req.body)
-    res.send('Hello');
+// app.post('/hello', (req,res) => {
+//     console.log(req.body)
+//     res.send('Hello');
+// })
+//
+// app.get('/hello/:name' , (req,res) =>{
+//     const { name } =req.params;
+//     res.send(`this is a test ${name}`)
+// })
+
+app.get('/api/articles/:name', async (req,res) => {
+    const {name} = req.params
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+    await client.connect()
+    const db = client.db('recipe-db')
+    const article = await db.collection('articles').findOne({name})
+    res.json(article)
 })
 
-app.get('/hello/:name' , (req,res) =>{
-    const { name } =req.params;
-    res.send(`this is a test ${name}`)
-})
 
 app.listen(8000, () => {
     console.log('server is listening on port 8000')
