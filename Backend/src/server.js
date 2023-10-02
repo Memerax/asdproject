d60f0a38fe9b68e873580f20fd50fcf35329abac
memerax <josephwhite@gmx.com>
2023-10-02 17:20:34 -0230

    Server testing

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
 
 const app = express();
-
+app.use(express.json());
 app.post('/hello', (req,res) => {
     console.log(req.body)
     res.send('Hello');
+})
+
+app.get('/hello/:name' , (req,res) =>{
+    const { name } =req.params;
+    res.send(`this is a test ${name}`)
 })
 
 app.listen(8000, () => {
